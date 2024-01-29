6f55db10935853b527f4b36e2dfedb506ca0e51c
Pmk10 <47448179+Pmk10@users.noreply.github.com>
2024-01-29 18:58:12 -0500

    Update layered_model_test.py

--- Assignment - 4/layered_model_test.py
+++ Assignment - 4/layered_model_test.py
@@ -5,7 +5,8 @@ import tensorflow as tf
 import pytest
 import os
 
-os.chdir('/Users/karthikeyanm/AppliedMathforDL')
+
+os.chdir('/Users/karthikeyanm/AppliedMathforDL/Assignment - 4')
 
 from layered_modelsolution import define_dense_model_single_layer, define_dense_model_with_hidden_layer
 from layered_modelsolution import fit_mnist_model_single_digit, evaluate_mnist_model_single_digit
